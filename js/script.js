690042a4fda84bc4e21cc3d698339655599d6d00
pavlo20031 <lesivpavlo999@gmail.com>
2026-01-14 21:57:53 +0200

    commit12321

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// Result
+const result = 5 + 5 + "5";
+console.log(result);
+console.log(typeof result);
+
+// Email
+const email = "lesivpavlo999@gmail.com";
+console.log(email.includes("@"));
+console.log(email.length);
+
+// Name
+const a = "My ";
+const b = "name ";
+const c = "is ";
+const fullName = a + b + c + "Viktor"
+console.log(fullName);
+
+// User
+const userName = "Ігорю";
+const payment = 600;
+alert(`Дякуємо, ${userName}! До сплати ${payment} гривень`)
